94913ee4c0fb7b9d9ada095ce37b6e97f4085fd8
lotusirous <5105237+lotusirous@users.noreply.github.com>
2023-01-15 14:55:58 +0700

    fetcher: Add github clone and summary

--- fetcher/github/github.go
+++ fetcher/github/github.go
@@ -1,7 +1,6 @@
 package github
 
 import (
-	"context"
 	"os"
 
 	"github.com/go-git/go-git/v5"
@@ -9,29 +8,55 @@ import (
 )
 
 // New inits the github fetcher
-func New(pattern string) core.GitFetcher {
-	return &githubFetcher{pattern}
+func New(dir, pattern string) core.GitFetcher {
+	return &github{dir, pattern}
 }
 
-type githubFetcher struct {
-	pattern string
+type github struct {
+	dir, pattern string
 }
 
-func (gh *githubFetcher) Clone(ctx context.Context, remoteURL string) (string, func(), error) {
-	dir, err := os.MkdirTemp("", gh.pattern)
+// Summarize extracts meta data from a git repo
+func (gh *github) Summarize(dir string) (*core.GitSummary, error) {
+	repo, err := git.PlainOpen(dir)
 	if err != nil {
-		return "", func() {}, err
+		return nil, err
+	}
+	ref, err := repo.Head()
+	if err != nil {
+		return nil, err
 	}
 
-	cleanup := func() {
-		os.RemoveAll(dir)
+	commit, err := repo.CommitObject(ref.Hash())
+	if err != nil {
+		return nil, err
 	}
+
+	out := new(core.GitSummary)
+	out.CommitHash = ref.Hash().String()
+	out.Branch = ref.Name().String()
+	out.CommitTime = commit.Committer.When
+
+	return out, nil
+}
+
+// Clone fetches the remote repo to local dir.
+// The caller will clean up the temp dir.
+func (gh *github) Clone(remoteURL string) (string, func() error, error) {
+	dir, err := os.MkdirTemp(gh.dir, gh.pattern)
+	if err != nil {
+		return "", func() error { return nil }, err
+	}
+	cleanup := func() error {
+		return os.RemoveAll(dir)
+	}
+
 	_, err = git.PlainClone(dir, false, &git.CloneOptions{
 		URL:               remoteURL,
 		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
 	})
 	if err != nil {
-		return "", cleanup, err
+		return dir, cleanup, err
 	}
 
 	return dir, cleanup, nil
--- fetcher/github/github_test.go
+++ fetcher/github/github_test.go
@@ -3,6 +3,8 @@ package github
 import (
 	"context"
 	"testing"
+
+	"github.com/lotusirous/codescan/core"
 )
 
 var noContext = context.TODO()
@@ -10,18 +12,31 @@ var noContext = context.TODO()
 func TestGitFetcher(t *testing.T) {
 
 	cases := []struct {
-		URL string
+		URL  string
+		Want *core.GitSummary
 	}{
 		{
 			URL: "https://github.com/octocat/Hello-World",
+			Want: &core.GitSummary{
+				Branch:     "refs/heads/master",
+				CommitHash: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+			},
+		},
+		{
+			URL: "https://github.com/octocat/hello-worId",
+			Want: &core.GitSummary{
+				Branch:     "refs/heads/master",
+				CommitHash: "7e068727fdb347b685b658d2981f8c85f7bf0585",
+			},
 		},
 	}
 
-	gh := githubFetcher{"testdata"}
+	gh := &github{dir: "testdata", pattern: "gh"}
 
+	t.Parallel()
 	for _, tt := range cases {
 		t.Run(tt.URL, func(t *testing.T) {
-			dir, cleanup, err := gh.Clone(noContext, tt.URL)
+			dir, cleanup, err := gh.Clone(tt.URL)
 			if err != nil {
 				t.Error(err)
 			}
@@ -29,8 +44,25 @@ func TestGitFetcher(t *testing.T) {
 			if dir == "" {
 				t.Error("Empty tempdir after clone")
 			}
-			t.Logf("clone to dir: %s", dir)
-			cleanup()
+
+			got, err := gh.Summarize(dir)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			if got.CommitHash != tt.Want.CommitHash {
+				t.Errorf("commit hash got: %s - want: %s", got.CommitHash, tt.Want.CommitHash)
+			}
+
+			if got.Branch != tt.Want.Branch {
+				t.Errorf("branch got: %s - want: %s", got.Branch, tt.Want.Branch)
+			}
+
+			if err := cleanup(); err != nil {
+				t.Error(err)
+			}
+
 		})
 	}
 }
