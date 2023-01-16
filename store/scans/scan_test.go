ecff948cffa07a75ce5d9192d894a23dfc7b2012
lotusirous <5105237+lotusirous@users.noreply.github.com>
2023-01-17 06:32:39 +0700

    store: Add scan update, find enqueued tests

--- store/scans/scan_test.go
+++ store/scans/scan_test.go
@@ -3,6 +3,7 @@ package scans
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/lotusirous/codescan/core"
 	"github.com/lotusirous/codescan/store/db/dbtest"
@@ -39,9 +40,44 @@ func testScanCreate(store *scanStore) func(t *testing.T) {
 		if sc.ID == 0 {
 			t.Errorf("Want scan id  assigned, got %d", sc.ID)
 		}
-		t.Run("count", testScanCount(store))
+		t.Run("Count", testScanCount(store))
 		t.Run("Find", testScanFind(store, sc))
+		t.Run("FindEnqueued", testScanFindEnqueued(store))
+		t.Run("Update", testScanUpdate(store, sc))
+	}
+}
 
+func testScanFindEnqueued(store *scanStore) func(*testing.T) {
+	return func(t *testing.T) {
+		out, err := store.FindEnqueued(noContext)
+		if err != nil {
+			t.Error(err)
+		}
+		if got := len(out); got != 1 {
+			t.Errorf("Want 1 enqueued rows got: %d", len(out))
+		}
+	}
+}
+
+func testScanUpdate(store *scanStore, sc *core.Scan) func(*testing.T) {
+	return func(t *testing.T) {
+		sc.Status = core.StatusInProgress
+		sc.EnqueuedAt = time.Now().Unix()
+		err := store.Update(noContext, sc)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out, err := store.Find(noContext, sc.ID)
+		if err != nil {
+			t.Error(err)
+		}
+		if got := out.Status; got != core.StatusInProgress {
+			t.Errorf("Want status updated in progress got: %s", got)
+		}
+		if out.EnqueuedAt == 0 {
+			t.Error("Want scan enqueued time not 0")
+		}
 	}
 }
 
@@ -63,12 +99,12 @@ func testScanFind(store *scanStore, sc *core.Scan) func(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		} else {
-			t.Run("Fields", testScan(r))
+			t.Run("Fields", testScanFields(r))
 		}
 	}
 }
 
-func testScan(sc *core.Scan) func(t *testing.T) {
+func testScanFields(sc *core.Scan) func(t *testing.T) {
 	return func(t *testing.T) {
 		if got, want := sc.Status, core.StatusQueued; got != want {
 			t.Errorf("Want scan status %q, got %q", want, got)
